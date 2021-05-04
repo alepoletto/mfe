daae6b2f2f4638fd62089bf07f36ccc00886ab78
Alexandre Poletto <alexandrepoletto@gmail.com>
2021-05-04 14:53:23 -0700

    trigger uild 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 const App = () => (
   <div>
-    <h1>Hello amigo</h1>
+    <h1>Hello amigo 2</h1>
     <hr />
     <MarketingApp />
   </div>

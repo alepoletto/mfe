c3172c2637355fe7091e77d91ee367d30be58882
Alexandre Poletto <alexandrepoletto@gmail.com>
2021-05-04 17:40:35 -0700

    build

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -77,16 +77,14 @@ export default function Album() {
               variant="h2"
               align="center"
               color="textPrimary"
-              gutterBottom
-            >
-              Home Page
+              gutterBottom>
+              Home Page Lol
             </Typography>
             <Typography
               variant="h5"
               align="center"
               color="textSecondary"
-              paragraph
-            >
+              paragraph>
               Something short and leading about the collection below—its
               contents, the creator, etc. Make it short and sweet, but not too
               short so folks don&apos;t simply skip over it entirely.
@@ -154,8 +152,7 @@ export default function Album() {
           variant="subtitle1"
           align="center"
           color="textSecondary"
-          component="p"
-        >
+          component="p">
           Something here to give the footer a purpose!
         </Typography>
         <Copyright />

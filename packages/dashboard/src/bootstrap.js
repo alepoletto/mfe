8043d3565313532b7153c36c10730456beeb4b1f
Alexandre Poletto <alexandrepoletto@gmail.com>
2021-05-31 18:42:17 -0700

    vamo 2

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -5,7 +5,7 @@ const mount = (el) => {
   const app = createApp(Dashboard);
   app.mount(el);
 };
-
+//any
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_dashboard-dev-root');
   if (devRoot) {
